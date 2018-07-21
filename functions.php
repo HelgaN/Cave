bdf5950c17d107a010b4662c00865c4f21261b4e
HelgaN <ola14@tut.by>
2018-07-21 04:31:49 +0300

    Task 3.1

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+ function includeTemplate($name, $data_array, $param) {
+     if (file_exists("templates/" . $name . ".php")) {
+         ob_start();
+         extract($data_array);
+         require "templates/" . $name . ".php";
+         return ob_get_clean();
+     } else {
+         return "";
+     }
+ };
